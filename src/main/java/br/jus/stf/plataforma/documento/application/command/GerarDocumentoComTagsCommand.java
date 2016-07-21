794693338fce7ee6e7666240532890ee5b20ff3e
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-07-21 13:47:11 -0300

    Atualizando para o spring-boot 1.4.0.RC1

--- src/main/java/br/jus/stf/plataforma/documento/application/command/GerarDocumentoComTagsCommand.java
+++ src/main/java/br/jus/stf/plataforma/documento/application/command/GerarDocumentoComTagsCommand.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import javax.validation.constraints.NotNull;
 
-import org.elasticsearch.common.lang3.Validate;
+import org.apache.commons.lang3.Validate;
 
 import com.wordnik.swagger.annotations.ApiModel;
 import com.wordnik.swagger.annotations.ApiModelProperty;
--- src/test/java/br/jus/stf/plataforma/documento/AbstractDocumentoIntegrationTests.java
+++ src/test/java/br/jus/stf/plataforma/documento/AbstractDocumentoIntegrationTests.java
@@ -1,14 +1,10 @@
 package br.jus.stf.plataforma.documento;
 
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.WebIntegrationTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import br.jus.stf.core.framework.testing.IntegrationTestsSupport;
 
-@SpringApplicationConfiguration(ApplicationContextInitializer.class)
-@ActiveProfiles("test")
-@WebIntegrationTest({"server.port:0", "eureka.client.enabled:false"})
+@SpringBootTest(value = {"server.port:0", "eureka.client.enabled:false"}, classes = ApplicationContextInitializer.class)
 public abstract class AbstractDocumentoIntegrationTests extends IntegrationTestsSupport {
 
 }
