c077f615d5ce692dc693587f238ac4d30cd84e0d
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-10-06 19:39:59 -0300

    Alterando a forma de carregar os certificados, pois estava dando erro quando rodava no docker.

--- src/main/java/br/jus/stf/db/migration/V1_4__Carga_certificado_digital.java
+++ src/main/java/br/jus/stf/db/migration/V1_4__Carga_certificado_digital.java
@@ -11,6 +11,8 @@ import java.util.List;
 
 import org.apache.commons.io.IOUtils;
 import org.flywaydb.core.api.migration.spring.SpringJdbcMigration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.support.SqlLobValue;
 import org.springframework.stereotype.Component;
@@ -24,16 +26,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class V1_4__Carga_certificado_digital implements SpringJdbcMigration {
 
+    @Autowired
+    private ResourceLoader resourceLoader;
+    
     @Override
     public void migrate(JdbcTemplate jdbcTemplate) throws Exception {
         String command =
                 "insert into documents.certificado_digital (SEQ_CERTIFICADO_DIGITAL, DSC_CERTIFICADO_DIGITAL, COD_SERIAL, TXT_CERTIFICADO_DIGITAL, DAT_VALIDADE_INICIAL, DAT_VALIDADE_FINAL, SEQ_CERTIFICADO_EMISSOR, TIP_CERTIFICADO_DIGITAL, TIP_PKI) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
         List<String> certificadosParameters = Arrays.asList(
-                "cn=Autoridade Certificadora Raiz Brasileira v2 ou=Instituto Nacional de Tecnologia da Informacao – ITI o=ICP-Brasil c=BR, 01, certification/pkis/icp-brasil/acrb-v2.cer, 21/06/2010 17:04:57, 21/06/2023 17:04:57, 0, R, ICP_BRASIL",
-                "cn=Autoridade Certificadora da Justica v4 ou=Autoridade Certificadora Raiz Brasileira v2 o=ICP-Brasil c=BR, 09, certification/pkis/icp-brasil/ac-jus-v4.cer, 22/11/2011 15:15:01, 22/11/2021 15:15:01, 1, A, ICP_BRASIL",
-                "cn=AC CAIXA-JUS v2 ou=Autoridade Certificadora da Justica - AC-JUS o=ICP-Brasil c=BR, 03, certification/pkis/icp-brasil/ac-caixa-jus-v2.cer, 28/12/2011 11:03:21, 28/12/2019 11:03:21, 1, A, ICP_BRASIL",
-                "cn=AC CAIXA v2 ou=Autoridade Certificadora Raiz Brasileira v2 o=ICP-Brasil c=BR, 0b, certification/pkis/icp-brasil/ac-caixa-v2.cer, 02/12/2011 10:16:53, 02/12/2021 10:16:53, 3, A, ICP_BRASIL",
-                "cn=AC CAIXA PF v2 ou=Caixa Economica Federal o=ICP-Brasil c=BR, 28eea57c362904d8, certification/pkis/icp-brasil/ac-caixa-pf-v2.cer, 23/12/2011 11:52:58, 21/12/2019 11:52:58, 1, A, ICP_BRASIL");
+                "cn=Autoridade Certificadora Raiz Brasileira v2 ou=Instituto Nacional de Tecnologia da Informacao – ITI o=ICP-Brasil c=BR, 01, classpath:certification/pkis/icp-brasil/acrb-v2.cer, 21/06/2010 17:04:57, 21/06/2023 17:04:57, 0, R, ICP_BRASIL",
+                "cn=Autoridade Certificadora da Justica v4 ou=Autoridade Certificadora Raiz Brasileira v2 o=ICP-Brasil c=BR, 09, classpath:certification/pkis/icp-brasil/ac-jus-v4.cer, 22/11/2011 15:15:01, 22/11/2021 15:15:01, 1, A, ICP_BRASIL",
+                "cn=AC CAIXA-JUS v2 ou=Autoridade Certificadora da Justica - AC-JUS o=ICP-Brasil c=BR, 03, classpath:certification/pkis/icp-brasil/ac-caixa-jus-v2.cer, 28/12/2011 11:03:21, 28/12/2019 11:03:21, 1, A, ICP_BRASIL",
+                "cn=AC CAIXA v2 ou=Autoridade Certificadora Raiz Brasileira v2 o=ICP-Brasil c=BR, 0b, classpath:certification/pkis/icp-brasil/ac-caixa-v2.cer, 02/12/2011 10:16:53, 02/12/2021 10:16:53, 3, A, ICP_BRASIL",
+                "cn=AC CAIXA PF v2 ou=Caixa Economica Federal o=ICP-Brasil c=BR, 28eea57c362904d8, classpath:certification/pkis/icp-brasil/ac-caixa-pf-v2.cer, 23/12/2011 11:52:58, 21/12/2019 11:52:58, 1, A, ICP_BRASIL");
 
         for (String certificadoParameters : certificadosParameters) {
             Long certificadoId =
@@ -50,7 +55,7 @@ public class V1_4__Carga_certificado_digital implements SpringJdbcMigration {
         InputStream stream = null;
 
         try {
-            stream = ClassLoader.getSystemClassLoader().getResourceAsStream(parameters[2]);
+            stream = resourceLoader.getResource(parameters[2]).getInputStream();
             SqlLobValue arquivo = new SqlLobValue(stream, stream.available());
             SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
             Date validadeInicio = formato.parse(parameters[3]);
