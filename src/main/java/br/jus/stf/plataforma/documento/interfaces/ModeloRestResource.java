539190f1d06684bad52edb6bfbaedfc43d0b9ebf
Rafael.Alencar <rafaelesdras@gmail.com>
2016-10-27 20:25:01 -0200

    Adequação de URLs nas APIs de Documento, Modelo e TipoDocumento.

--- src/main/java/br/jus/stf/plataforma/documento/interfaces/ModeloRestResource.java
+++ src/main/java/br/jus/stf/plataforma/documento/interfaces/ModeloRestResource.java
@@ -23,12 +23,15 @@ import com.wordnik.swagger.annotations.ApiOperation;
 import br.jus.stf.core.shared.documento.DocumentoId;
 import br.jus.stf.core.shared.documento.ModeloId;
 import br.jus.stf.core.shared.documento.TipoDocumentoId;
+import br.jus.stf.plataforma.documento.domain.DocumentoService;
 import br.jus.stf.plataforma.documento.domain.model.ConteudoDocumento;
 import br.jus.stf.plataforma.documento.domain.model.DocumentoRepository;
 import br.jus.stf.plataforma.documento.domain.model.Modelo;
 import br.jus.stf.plataforma.documento.domain.model.ModeloRepository;
+import br.jus.stf.plataforma.documento.domain.model.Tag;
 import br.jus.stf.plataforma.documento.interfaces.dto.ModeloDto;
 import br.jus.stf.plataforma.documento.interfaces.dto.ModeloDtoAssembler;
+import br.jus.stf.plataforma.documento.interfaces.dto.TagDto;
 
 /**
  * Serviços Rest de Modelo.
@@ -40,76 +43,100 @@ import br.jus.stf.plataforma.documento.interfaces.dto.ModeloDtoAssembler;
 @RequestMapping("/api/modelos")
 public class ModeloRestResource {
 
-	@Autowired
-	private ModeloRepository modeloRepository;
+    @Autowired
+    private ModeloRepository modeloRepository;
 
-	@Autowired
-	private ModeloDtoAssembler modeloDtoAssembler;
-	
-	@Autowired
-	private DocumentoRepository documentoRepository;
+    @Autowired
+    private ModeloDtoAssembler modeloDtoAssembler;
 
-	/**
-	 * @return
-	 */
-	@ApiOperation("Recupera os modelos existentes")
-	@RequestMapping(value = "", method = RequestMethod.GET)
-	public List<ModeloDto> listar() {
-		List<Modelo> modelos = modeloRepository.findAll();
-		return modelos.stream().map(modeloDtoAssembler::toDto).collect(Collectors.toList());
-	}
+    @Autowired
+    private DocumentoRepository documentoRepository;
 
-	/**
-	 * @param tiposDocumento
-	 * @return
-	 */
-	/**
-	 * @param tiposDocumento
-	 * @return
-	 */
-	@ApiOperation("Recupera os modelos com os tipos de documentos especificados")
-	@RequestMapping(value = "/por-tipos-documento", method = RequestMethod.POST)
-	public List<ModeloDto> consultarPorTiposDocumento(@RequestBody List<Long> tiposDocumento) {
-		List<Modelo> modelos = modeloRepository.findByTiposDocumento(
-				tiposDocumento.stream().map(TipoDocumentoId::new).collect(Collectors.toList()));
-		return modelos.stream().map(modeloDtoAssembler::toDto).collect(Collectors.toList());
-	}
-	
-	/**
-	 * @param id
-	 * @return
-	 */
-	@ApiOperation("Recupera os dados de um modelo")
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ModeloDto consultar(@PathVariable("id") Long id) {
-		Modelo modelo = modeloRepository.findOne(new ModeloId(id));
-		return modeloDtoAssembler.toDto(modelo);
-	}
+    @Autowired
+    private DocumentoService documentoService;
 
-	/**
-	 * @param modeloId
-	 * @return
-	 * @throws IOException
-	 */
-	@ApiOperation("Recupera o conteúdo de um documento de modelo")
-	@RequestMapping(value = "/{modeloId}/documento/conteudo.docx", method = RequestMethod.GET)
-	public ResponseEntity<InputStreamResource> recuperarConteudo(@PathVariable("modeloId") Long modeloId)
-	        throws IOException {
-		Modelo modelo = modeloRepository.findOne(new ModeloId(modeloId));
-		ConteudoDocumento documento = documentoRepository.download(new DocumentoId(modelo.template().toLong()));
-		byte[] bytes = IOUtils.toByteArray(documento.stream());
-		InputStreamResource isr = new InputStreamResource(new ByteArrayInputStream(bytes));
-		HttpHeaders headers = createResponseHeaders(new Long(bytes.length));
-		return new ResponseEntity<>(isr, headers, HttpStatus.OK);
+    /**
+     * @return
+     */
+    @ApiOperation("Recupera os modelos existentes")
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public List<ModeloDto> listar() {
+        List<Modelo> modelos = modeloRepository.findAll();
 
-	}
+        return modelos.stream()
+                .map(modeloDtoAssembler::toDto)
+                .collect(Collectors.toList());
+    }
 
-	private HttpHeaders createResponseHeaders(Long tamanho) {
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentLength(tamanho);
-		headers.setContentType(
-		        MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.wordprocessingml.document"));
-		return headers;
-	}
+    /**
+     * @param tiposDocumento Tipos de documentos para filtrar os modelos.
+     * @return Modelos associados aos tipos de documentos.
+     */
+    @ApiOperation("Recupera os modelos com os tipos de documentos especificados.")
+    @RequestMapping(value = "/tipos-documento", method = RequestMethod.POST)
+    public List<ModeloDto> consultarPorTiposDocumento(@RequestBody List<Long> tiposDocumento) {
+        List<Modelo> modelos = modeloRepository.findByTiposDocumento(
+                tiposDocumento.stream()
+                        .map(TipoDocumentoId::new)
+                        .collect(Collectors.toList()));
+
+        return modelos.stream().map(modeloDtoAssembler::toDto).collect(Collectors.toList());
+    }
+
+    /**
+     * @param modeloId Identificador do modelo procurado.
+     * @return Dto que representa o modelo.
+     */
+    @ApiOperation("Recupera os dados de um modelo.")
+    @RequestMapping(value = "/{modeloId}", method = RequestMethod.GET)
+    public ModeloDto consultar(@PathVariable("modeloId") Long modeloId) {
+        Modelo modelo = modeloRepository.findOne(new ModeloId(modeloId));
+
+        return modeloDtoAssembler.toDto(modelo);
+    }
+
+    /**
+     * @param modeloId Identificador do modelo procurado.
+     * @return Stream com o conteúdo do modelo.
+     * @throws IOException
+     */
+    @ApiOperation("Recupera o conteúdo de um documento de modelo.")
+    @RequestMapping(value = "/{modeloId}/conteudo.docx", method = RequestMethod.GET)
+    public ResponseEntity<InputStreamResource> recuperarConteudo(@PathVariable("modeloId") Long modeloId)
+            throws IOException {
+        Modelo modelo = modeloRepository.findOne(new ModeloId(modeloId));
+        ConteudoDocumento documento = documentoRepository.download(new DocumentoId(modelo.template().toLong()));
+        byte[] bytes = IOUtils.toByteArray(documento.stream());
+        InputStreamResource isr = new InputStreamResource(new ByteArrayInputStream(bytes));
+        HttpHeaders headers = createResponseHeaders(Long.valueOf(bytes.length));
+
+        return new ResponseEntity<>(isr, headers, HttpStatus.OK);
+    }
+
+    private HttpHeaders createResponseHeaders(Long tamanho) {
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setContentLength(tamanho);
+        headers.setContentType(
+                MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.wordprocessingml.document"));
+
+        return headers;
+    }
+
+    /**
+     * @param modeloId Identificador do modelo que possui o template para extração das tags.
+     * @return Lista de dtos com as tags extraídas do template do modelo.
+     */
+    @ApiOperation("Extrai as tags de um template de um modelo.")
+    @RequestMapping(value = "/{modeloId}/tags", method = RequestMethod.GET)
+    public List<TagDto> extrairTags(@PathVariable("modeloId") Long modeloId) {
+        Modelo modelo = modeloRepository.findOne(new ModeloId(modeloId));
+        ConteudoDocumento conteudoDocumento = documentoRepository.download(modelo.template());
+        List<Tag> tags = documentoService.extrairTags(conteudoDocumento);
+
+        return tags.stream()
+                .map(t -> new TagDto(t.nome()))
+                .collect(Collectors.toList());
+    }
 
 }
--- src/test/java/br/jus/stf/plataforma/documento/DocumentoIntegrationTests.java
+++ src/test/java/br/jus/stf/plataforma/documento/DocumentoIntegrationTests.java
@@ -30,129 +30,141 @@ import br.jus.stf.plataforma.AbstractIntegrationTests;
 import br.jus.stf.plataforma.documento.interfaces.dto.DocumentoTemporarioDto;
 
 public class DocumentoIntegrationTests extends AbstractIntegrationTests {
-	
-	private String dividirDocumentoCommand;
-	private String intervaloJson;
-	
-	@Before
-	public void setUp() {
-		dividirDocumentoCommand = "{\"documentoId\": %d, \"intervalos\": [%s]}";
-		intervaloJson = "{\"paginaInicial\": %d, \"paginaFinal\": %d}";
-	}
-	
-	@Test
-	@WithMockOauth2User(value = "peticionador", components = "upload-documento-assinado")
-	public void enviarArquivoSemAssinatura() throws Exception {
-		String nomeArquivo = "teste_arq_temp.pdf";
-		String mime = "application/pdf";
-		String caminho = "pdf/padraoAD-V2.pdf";
-		
-		byte[] arquivo = IOUtils.toByteArray(new ClassPathResource(caminho).getInputStream());
 
-		MockMultipartFile mockArquivo = new MockMultipartFile("file", nomeArquivo, mime, arquivo);
-		
-		mockMvc.perform(fileUpload("/api/documentos/upload/assinado")
-					.file(mockArquivo)
-					.contentType(MediaType.MULTIPART_FORM_DATA)
-					.content(arquivo))
-			.andExpect(status().is4xxClientError());
-	}
-	
-	@Test
-	@WithMockOauth2User(value = "peticionador", components = {"upload-documento-assinado", "salvar-documentos"})
-	public void enviarArquivoAssinado() throws Exception {
-		String nomeArquivo = "teste_arq_temp.pdf";
-		String mime = "application/pdf";
-		String caminho = "certification/pdf-de-teste-assinado-02.pdf";
-		
-		byte[] arquivo = IOUtils.toByteArray(new ClassPathResource(caminho).getInputStream());
+    private String dividirDocumentoCommand;
 
-		MockMultipartFile mockArquivo = new MockMultipartFile("file", nomeArquivo, mime, arquivo);
-		
-		String idTemp = mockMvc.perform(fileUpload("/api/documentos/upload/assinado")
-					.file(mockArquivo)
-					.contentType(MediaType.MULTIPART_FORM_DATA)
-					.content(arquivo))
-			.andExpect(status().is2xxSuccessful())
-			.andReturn().getResponse().getContentAsString();
-		
-		String json = mockMvc.perform(post("/api/documentos")
-					.contentType(MediaType.APPLICATION_JSON)
-					.content("{\"idsDocumentosTemporarios\" : [\"" + idTemp + "\"]}"))
-			.andExpect(status().is2xxSuccessful())
-			.andReturn().getResponse().getContentAsString();
-		
-		JavaType type = TypeFactory.defaultInstance().constructParametrizedType(ArrayList.class, ArrayList.class, DocumentoTemporarioDto.class);
-		List<DocumentoTemporarioDto> dtos = new ObjectMapper().readValue(json, type); 
-	 
-		mockMvc.perform(get("/api/documentos/" + dtos.get(0).getDocumentoId() + "/conteudo"))
-			.andExpect(status().isOk())
-			.andExpect(content().bytes(arquivo));
-	}
-	
-	@Test
-	@WithMockOauth2User(value = "organizador-pecas", components = {"upload-documento", "salvar-documentos", "dividir-documentos-completamente", "unir-documentos"})
-	public void dividirEUnirDocumentos() throws Exception {
-		Integer documentoId = fazerUploadDocumento();
-		
-		String dividirDocumentoCommandJsonStr = String.format(dividirDocumentoCommand,
-				documentoId, String.format(intervaloJson, 1, 7) + ", " + String.format(intervaloJson, 8, 14));
-		String documentosDivididos = mockMvc.perform(post("/api/documentos/dividir").contentType(MediaType.APPLICATION_JSON)
-				.content(dividirDocumentoCommandJsonStr)).andExpect(status().is2xxSuccessful())
-				.andExpect(jsonPath("$", hasSize(2))).andReturn().getResponse()
-				.getContentAsString();
-		
-		String unirDocumentosCommand = "{\"idsDocumentos\": " + documentosDivididos + "}";
-		
-		mockMvc.perform(post("/api/documentos/unir").contentType(MediaType.APPLICATION_JSON)
-				.content(unirDocumentosCommand)).andExpect(status().is2xxSuccessful())
-				.andReturn().getResponse()
-				.getContentAsString();
-	}
-	
-	@Test
-	@WithMockOauth2User(value = "organizador-pecas", components = {"upload-documento", "salvar-documentos", "dividir-documentos-completamente"})
-	public void dividirDocumentoIntervalosInvalidos() throws Exception {
-		Integer documentoId = fazerUploadDocumento();
-		
-		String dividirDocumentoCommandJsonStr = String.format(dividirDocumentoCommand,
-				documentoId, String.format(intervaloJson, 1, 7) + ", " + String.format(intervaloJson, 9, 14));
-		String json = mockMvc.perform(post("/api/documentos/dividir").contentType(MediaType.APPLICATION_JSON)
-				.content(dividirDocumentoCommandJsonStr)).andExpect(status().is4xxClientError()).andReturn().getResponse().getContentAsString();
-		String erro = JsonPath.read(json, "$.errors[0].message");
-		Assert.assertEquals("Nem todas as páginas do documento foram contempladas.", erro);
-	}
-	
-	@Test
-	@WithMockOauth2User("peticionador")
-	public void consultarDocumentoPorId() throws Exception {
-		mockMvc.perform(get("/api/documentos/1")).andExpect(status().isOk())
-		.andExpect(jsonPath("$.documentoId", is(1)));
-	}
-	
-	private Integer fazerUploadDocumento() throws Exception {
-		String nomeArquivo = "pdf-14-pgs.pdf";
-		String mime = "application/pdf";
-		String caminho = "pdf/pdf-14-pgs.pdf";
-		
-		byte[] arquivo = IOUtils.toByteArray(new ClassPathResource(caminho).getInputStream());
+    private String intervaloJson;
 
-		MockMultipartFile mockArquivo = new MockMultipartFile("file", nomeArquivo, mime, arquivo);
-		
-		String idTemp = mockMvc.perform(fileUpload("/api/documentos/upload")
-					.file(mockArquivo)
-					.contentType(MediaType.MULTIPART_FORM_DATA)
-					.content(arquivo))
-			.andExpect(status().is2xxSuccessful())
-			.andReturn().getResponse().getContentAsString();
+    @Before
+    public void setUp() {
+        dividirDocumentoCommand = "{\"documentoId\": %d, \"intervalos\": [%s]}";
+        intervaloJson = "{\"paginaInicial\": %d, \"paginaFinal\": %d}";
+    }
 
-		String salvarDocumentosCommand = String.format("{\"idsDocumentosTemporarios\": [\"%s\"]}", idTemp);
+    @Test
+    @WithMockOauth2User(value = "peticionador", components = "upload-documento-assinado")
+    public void enviarArquivoSemAssinatura() throws Exception {
+        String nomeArquivo = "teste_arq_temp.pdf";
+        String mime = "application/pdf";
+        String caminho = "pdf/padraoAD-V2.pdf";
 
-		String documentosSalvos = mockMvc.perform(
-				post("/api/documentos").contentType(MediaType.APPLICATION_JSON).content(salvarDocumentosCommand))
-				.andExpect(status().is2xxSuccessful()).andReturn().getResponse().getContentAsString();
-		
-		Integer documentoId = JsonPath.read(documentosSalvos, "$[0].documentoId");
-		return documentoId;
-	}
+        byte[] arquivo = IOUtils.toByteArray(new ClassPathResource(caminho).getInputStream());
+
+        MockMultipartFile mockArquivo = new MockMultipartFile("file", nomeArquivo, mime, arquivo);
+
+        mockMvc.perform(fileUpload("/api/documentos/conteudo/assinado")
+                .file(mockArquivo)
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .content(arquivo))
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    @WithMockOauth2User(value = "peticionador", components = { "upload-documento-assinado", "salvar-documentos" })
+    public void enviarArquivoAssinado() throws Exception {
+        String nomeArquivo = "teste_arq_temp.pdf";
+        String mime = "application/pdf";
+        String caminho = "certification/pdf-de-teste-assinado-02.pdf";
+
+        byte[] arquivo = IOUtils.toByteArray(new ClassPathResource(caminho).getInputStream());
+
+        MockMultipartFile mockArquivo = new MockMultipartFile("file", nomeArquivo, mime, arquivo);
+
+        String idTemp = mockMvc.perform(fileUpload("/api/documentos/conteudo/assinado")
+                .file(mockArquivo)
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .content(arquivo))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn().getResponse().getContentAsString();
+
+        String json = mockMvc.perform(post("/api/documentos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"idsDocumentosTemporarios\" : [\"" + idTemp + "\"]}"))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn().getResponse().getContentAsString();
+
+        JavaType type = TypeFactory.defaultInstance().constructParametrizedType(ArrayList.class, ArrayList.class,
+                DocumentoTemporarioDto.class);
+        List<DocumentoTemporarioDto> dtos = new ObjectMapper().readValue(json, type);
+
+        mockMvc.perform(get("/api/documentos/" + dtos.get(0).getDocumentoId() + "/conteudo"))
+                .andExpect(status().isOk())
+                .andExpect(content().bytes(arquivo));
+    }
+
+    @Test
+    @WithMockOauth2User(value = "organizador-pecas", components = { "upload-documento", "salvar-documentos",
+            "dividir-documentos-completamente", "unir-documentos" })
+    public void dividirEUnirDocumentos() throws Exception {
+        Integer documentoId = fazerUploadDocumento();
+
+        String dividirDocumentoCommandJsonStr = String.format(dividirDocumentoCommand,
+                documentoId, String.format(intervaloJson, 1, 7) + ", " + String.format(intervaloJson, 8, 14));
+        String documentosDivididos = mockMvc
+                .perform(post("/api/documentos/" + documentoId + "/divisao")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(dividirDocumentoCommandJsonStr))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$", hasSize(2))).andReturn().getResponse()
+                .getContentAsString();
+
+        String unirDocumentosCommand = "{\"idsDocumentos\": " + documentosDivididos + "}";
+
+        mockMvc.perform(post("/api/documentos/uniao")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(unirDocumentosCommand)).andExpect(status().is2xxSuccessful())
+                .andReturn().getResponse()
+                .getContentAsString();
+    }
+
+    @Test
+    @WithMockOauth2User(value = "organizador-pecas",
+            components = { "upload-documento", "salvar-documentos", "dividir-documentos-completamente" })
+    public void dividirDocumentoIntervalosInvalidos() throws Exception {
+        Integer documentoId = fazerUploadDocumento();
+
+        String dividirDocumentoCommandJsonStr = String.format(dividirDocumentoCommand,
+                documentoId, String.format(intervaloJson, 1, 7) + ", " + String.format(intervaloJson, 9, 14));
+        String json = mockMvc
+                .perform(post("/api/documentos/" + documentoId + "/divisao")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(dividirDocumentoCommandJsonStr))
+                .andExpect(status().is4xxClientError()).andReturn()
+                .getResponse().getContentAsString();
+        String erro = JsonPath.read(json, "$.errors[0].message");
+        Assert.assertEquals("Nem todas as páginas do documento foram contempladas.", erro);
+    }
+
+    @Test
+    @WithMockOauth2User("peticionador")
+    public void consultarDocumentoPorId() throws Exception {
+        mockMvc.perform(get("/api/documentos/1")).andExpect(status().isOk())
+                .andExpect(jsonPath("$.documentoId", is(1)));
+    }
+
+    private Integer fazerUploadDocumento() throws Exception {
+        String nomeArquivo = "pdf-14-pgs.pdf";
+        String mime = "application/pdf";
+        String caminho = "pdf/pdf-14-pgs.pdf";
+
+        byte[] arquivo = IOUtils.toByteArray(new ClassPathResource(caminho).getInputStream());
+
+        MockMultipartFile mockArquivo = new MockMultipartFile("file", nomeArquivo, mime, arquivo);
+
+        String idTemp = mockMvc.perform(fileUpload("/api/documentos/conteudo")
+                .file(mockArquivo)
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .content(arquivo))
+                .andExpect(status().is2xxSuccessful())
+                .andReturn().getResponse().getContentAsString();
+
+        String salvarDocumentosCommand = String.format("{\"idsDocumentosTemporarios\": [\"%s\"]}", idTemp);
+
+        String documentosSalvos = mockMvc.perform(
+                post("/api/documentos").contentType(MediaType.APPLICATION_JSON).content(salvarDocumentosCommand))
+                .andExpect(status().is2xxSuccessful()).andReturn().getResponse().getContentAsString();
+
+        Integer documentoId = JsonPath.read(documentosSalvos, "$[0].documentoId");
+        return documentoId;
+    }
 }
